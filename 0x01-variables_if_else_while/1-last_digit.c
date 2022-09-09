e0154082ed1cf4f63a7b5c70871207f5e6fcba38
Diamondcallist <Diamondcallist>
2022-09-09 12:02:02 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ l = n % 10;
 
 if (n % 1 > 5)
 {
-	printf("Last digit of %d is %d and is greater than 5\n", n, l);
+	printf("Last digit of %d is %d and is greater than 5 and not 0\n", n, l);
 }
 else if (l == 0)
 {
@@ -28,7 +28,7 @@ else if (l == 0)
 }
 else
 {
-	printf("Last digit of %d is %d and is greater than 5 and not 0\n", n, l);
+	printf("Last digit of %d is %d and is less than  and not 0\n", n, l);
 }
 return (0);
 }
