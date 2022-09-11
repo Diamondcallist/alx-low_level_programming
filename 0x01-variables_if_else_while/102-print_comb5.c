e55fd101552672b76138c5c3fd2387fd801fc82f
Diamondcallist <Diamondcallist>
2022-09-11 15:44:50 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,13 +10,10 @@ int main(void)
 	int i, p;
 
 	for (i = 0; i < 100; i++)
-	
 	{
 		for (p = 0; p < 100; i++)
-
 		{
 			if (p > i)
-
 			{
 				putchar((i / 10) + '0');
 				putchar((i % 10) + '0');
@@ -24,21 +21,13 @@ int main(void)
 				putchar((p / 10) + '0');
 				putchar((p % 10) + '0');
 				if (i != 98)
-
 				{
 					putchar(',');
 					putchar(' ');
-
 				}
-
 			}
-
 		}
-
 	}
-
 	putchar('\n');
-
 	return (0);
-
 }
