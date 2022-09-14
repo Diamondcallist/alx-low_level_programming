1c6b96afdf643ae1a6219279213e7050fa961f29
Diamondcallist <Diamondcallist>
2022-09-13 21:01:04 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,9 +3,9 @@
 /**
  * print_to_98 - Prints all natural numbers
  * from n to 98.
- * 
+ *
  * @n: input number
- * 
+ *
  * Return: no return.
  */
 void print_to_98(int n)
@@ -16,7 +16,8 @@ void print_to_98(int n)
 		{
 			printf("%d, ", n);
 		}
-	}else if (n < 98)
+	}
+	else if (n < 98)
 	{
 		for (; n < 98; n++)
 		{
