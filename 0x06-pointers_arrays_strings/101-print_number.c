fa8a03735db813eb660a97cef56bd31f0c69a976
Diamondcallist <Diamondcallist>
2022-09-22 17:28:26 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
- * print_number - prints an integer
- * @n:interger to be printed
- *
- *
- */
+* print_number - prints an integer
+* @n:interger to be printed
+*
+*/
 void print_number(int n)
 {
 	unsigned int num =n;
