11a744250c51568904fe539ade570bb7a2ffa634
Diamondcallist <Diamondcallist>
2022-09-26 10:40:18 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _memset -   fills memory with a constant byte
- * @s: pointer block of memory to fill
- * @b: value to set
- * @n: bytes of the memory
- * Return: dest
+ * _memset - fills memory with a constant byte
+ * @s: address to memory block
+ * @b: char to be used
+ *
+ * Return: pointer to the memory block
  */
 
 char *_memset(char *s, char b, unsigned int n)
