030f84c49471200cbec0324cb8911410d8b7e4ca
Diamondcallist <Diamondcallist>
2022-10-10 11:59:13 -0700

    Task all

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef _DOG_H
-#define _DOG_H
+#ifndef _DOG_H_
+#define _DOG_H_
 
 /**
  * dog_t - Typedef for struct dog
@@ -21,10 +21,10 @@ struct dog
 	float age;
 	char *owner;
 };
-typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif /*DOG_H*/
+#endif
